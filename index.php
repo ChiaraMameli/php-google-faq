358f40c96ca0c5d9b095b65a97b39756bacfeb30
ChiaraMameli <mameli.chiara@libero.it>
2022-08-31 14:58:27 +0200

    created data

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$content = [
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+   ],
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+   ],
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<ol><li>ciccio</li>',
+    'answer' => '<ul><li>ciccio</li>',
+    'answer' => '<li>ciccio</li></ul>',
+    'answer' => '<li>ciccio</li></ol>',
+   ],
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+    'answer' => '<p>ciccio</p>',
+   ],
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+   ],
+   [
+    'question' => '<h2>pippo</h2>',
+    'answer' => '<p>ciccio</p>',
+   ]
+   ];
+
+   ?>

c1311bbfd2a2441a5f961ce97ae54ec7b38403d4
ChiaraMameli <mameli.chiara@libero.it>
2022-08-31 15:11:21 +0200

    added bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$content = [
+$questions = [
    [
     'question' => '<h2>pippo</h2>',
     'answer' => '<p>ciccio</p>',
@@ -19,10 +19,10 @@ $content = [
    [
     'question' => '<h2>pippo</h2>',
     'answer' => '<p>ciccio</p>',
-    'answer' => '<ol><li>ciccio</li>',
-    'answer' => '<ul><li>ciccio</li>',
-    'answer' => '<li>ciccio</li></ul>',
-    'answer' => '<li>ciccio</li></ol>',
+    'answer' => '<ol><li>ciccio<ul>',
+    'answer' => '<li>ciccio</li>',
+    'answer' => '<li>ciccio</li></ul></li>',
+    'answer' => '<li> ciccio</li></ol>',
    ],
    [
     'question' => '<h2>pippo</h2>',
@@ -41,3 +41,25 @@ $content = [
    ];
 
    ?>
+
+   <!DOCTYPE html>
+   <html lang="it">
+   <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Domande frequenti - Privacy e termini</title>
+   </head>
+   <body>
+    <main class="container">
+        <?php foreach($questions as $question) : ?>
+            <?php foreach($question as $value) : ?>
+                <?= $value ?>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </main>
+
+   </body>
+   </html>
